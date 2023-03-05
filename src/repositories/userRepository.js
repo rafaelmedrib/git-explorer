9967ec742f0c17115742faa5062e3b8334c1c687
Rafael Medeiros <rafaelmedrib@gmail.com>
2023-03-04 22:28:41 -0300

    user-repository
    adds the user repository crud

---
+++ src/repositories/userRepository.js
@@ -0,0 +1,34 @@
+const { User } = require('../models/User');
+
+class UserRepository {
+    async findAll() {
+        return User.findAll();
+    }
+
+    async findById(id) {
+        return User.findByPk(id);
+    }
+
+    async create(data) {
+        return User.create(data);
+    }
+
+    async update(id, data) {
+        const user = await User.findByPk(id);
+        if (user) {
+            return user.update(data);
+        }
+        return null;
+    }
+
+    async delete(id) {
+        const user = await User.findByPk(id);
+        if (user) {
+            await user.destroy();
+            return true;
+        }
+        return false;
+    }
+}
+
+module.exports = new UserRepository();
---
+++ test/repositories/userRepository.test.js
@@ -0,0 +1,136 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+
+const { User } = require('../../src/models/User');
+const userRepository = require('../../src/repositories/userRepository');
+
+describe('UserRepository', () => {
+    describe('findAll', () => {
+        it('should return all users', async () => {
+            const fakeUsers = [
+                {
+                    id: '123',
+                    email: 'user1@example.com',
+                    favorites: null
+                },
+                {
+                id: '456',
+                email: 'user2@example.com',
+                favorites: '789'
+                },
+            ];
+
+            const findAllFake = sinon.fake.returns(fakeUsers);
+            sinon.replace(User, 'findAll', findAllFake);
+
+            const users = await userRepository.findAll();
+            expect(users).to.deep.equal(fakeUsers);
+
+            sinon.restore();
+        });
+    });
+
+    describe('findById', () => {
+        it('should return the user with the specified id', async () => {
+            const id = '123';
+            const fakeUser = {id, email: 'user1@example.com', favorites: null};
+
+            const findByPkFake = sinon.fake.returns(fakeUser);
+            sinon.replace(User, 'findByPk', findByPkFake);
+
+            const user = await userRepository.findById(id);
+            expect(user).to.deep.equal(fakeUser);
+
+            sinon.restore();
+        });
+
+        it('should return null when the user is not found', async () => {
+            const id = '789';
+
+            const findByPkFake = sinon.fake.returns(null);
+            sinon.replace(User, 'findByPk', findByPkFake);
+
+            const user = await userRepository.findById(id);
+            expect(user).to.be.null;
+
+            sinon.restore();
+        });
+    });
+
+    describe('create', () => {
+        it('should create a new user', async () => {
+            const userData = {email: 'user3@example.com', favorites: '456'};
+            const fakeUser = {id: '789', ...userData};
+
+            const createFake = sinon.fake.returns(fakeUser);
+            sinon.replace(User, 'create', createFake);
+
+            const user = await userRepository.create(userData);
+            expect(user).to.deep.equal(fakeUser);
+
+            sinon.restore();
+        });
+    });
+
+    describe('update', () => {
+        it('should update the user with the specified id', async () => {
+            const id = '123';
+            const userData = {email: 'user1@example.com', favorites: '789', update: () => [1]};
+            const updatedUser = {id, ...userData};
+
+            const findByPkFake = sinon.fake.returns(updatedUser);
+            sinon.replace(User, 'findByPk', findByPkFake);
+
+            const updateFake = sinon.fake.returns([1]);
+            sinon.replace(updatedUser, 'update', updateFake);
+
+            const response = await userRepository.update(id, userData);
+            expect(response).to.deep.equal([1]);
+
+            sinon.restore();
+        });
+
+        it('should return null when the user is not found', async () => {
+            const id = '789';
+            const userData = {email: 'user3@example.com', favorites: '123'};
+
+            const findByPkFake = sinon.fake.returns(null);
+            sinon.replace(User, 'findByPk', findByPkFake);
+
+            const user = await userRepository.update(id, userData);
+            expect(user).to.be.null;
+
+            sinon.restore();
+        });
+    });
+    describe('delete', function () {
+        it('should delete the user specified id', async () => {
+            const id = '123';
+            const userData = {email: 'user1@example.com', favorites: '789', destroy: () => {}};
+            const updatedUser = {id, ...userData};
+
+            const findByPkFake = sinon.fake.returns(updatedUser);
+            sinon.replace(User, 'findByPk', findByPkFake);
+
+            const updateFake = sinon.fake.returns(true);
+            sinon.replace(updatedUser, 'destroy', updateFake);
+
+            const response = await userRepository.delete(id, userData);
+            expect(response).to.deep.equal(true);
+
+            sinon.restore();
+        });
+
+        it('should return false when the user is not found', async () => {
+            const id = '789';
+
+            const findByPkFake = sinon.fake.returns(null);
+            sinon.replace(User, 'findByPk', findByPkFake);
+
+            const response = await userRepository.delete(id);
+            expect(response).to.be.false;
+
+            sinon.restore();
+        });
+    });
+});
